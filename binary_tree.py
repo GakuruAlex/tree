bacc9866def4cfc4cf35ce9b65a80b4898f585eb
= <=>
2025-02-16 21:50:21 +0300

    OOP

--- binary_tree.py
+++ binary_tree.py
@@ -3,40 +3,62 @@ class BinaryTree:
         self.key = key
         self.left = None
         self.right = None
-def in_order_traversal(tree: BinaryTree)-> list:
-        if tree is None:
+    def in_order_traversal(self)-> list:
+            if self is None:
+                return []
+            else:
+                return (BinaryTree.in_order_traversal(self.left) + [self.key] + BinaryTree.in_order_traversal(self.right))
+    def pre_order_traversal(self)-> list:
+        if self is None:
             return []
         else:
-            return (in_order_traversal(tree.left) + [tree.key] + in_order_traversal(tree.right))
-def pre_order_traversal(tree: BinaryTree)-> list:
-    if tree is None:
-        return []
-    else:
-        return ([tree.key] +pre_order_traversal(tree.left)+ pre_order_traversal(tree.right))
-def post_order_traversal(tree: BinaryTree)-> list:
-    if tree is None:
-        return []
-    else:
-        return (post_order_traversal(tree.left)+ post_order_traversal(tree.right)+[tree.key])
-
-def binary_tree_from_tuple(tree_tuple):
-            if isinstance(tree_tuple, tuple) and len(tree_tuple) == 3:
-                node =BinaryTree(tree_tuple[1])
-                node.right = binary_tree_from_tuple(tree_tuple[2])
-                node.left = binary_tree_from_tuple(tree_tuple[0])
-            elif tree_tuple is None:
-                node = None
-            else:
-                node = BinaryTree(tree_tuple)
-            return node
+            return ([self.key] + BinaryTree.pre_order_traversal(self.left)+ BinaryTree.pre_order_traversal(self.right))
+    def post_order_traversal(self)-> list:
+        if self is None:
+            return []
+        else:
+            return (BinaryTree.post_order_traversal(self.left)+ BinaryTree.post_order_traversal(self.right)+[self.key])
+    def tuple_from_tree(self):
+        if self is None:
+            return None
+        if self.left is None and self.right is None:
+            return self.key
+        else:
+            return BinaryTree.tuple_from_tree(self.left),self.key, BinaryTree.tuple_from_tree(self.right)
+    def height_of_tree(self):
+        if self is None:
+            return 0
+        else:
+            return 1 + max(BinaryTree.height_of_tree(self.left), BinaryTree.height_of_tree(self.right))
+    def size_of_tree(self):
+        if self is None:
+            return 0
+        else:
+            return 1 + BinaryTree.size_of_tree(self.left) + BinaryTree.size_of_tree(self.right)
+    @staticmethod
+    def binary_tree_from_tuple(tree_tuple):
+                if isinstance(tree_tuple, tuple) and len(tree_tuple) == 3:
+                    node =BinaryTree(tree_tuple[1])
+                    node.right = BinaryTree.binary_tree_from_tuple(tree_tuple[2])
+                    node.left = BinaryTree.binary_tree_from_tuple(tree_tuple[0])
+                elif tree_tuple is None:
+                    node = None
+                else:
+                    node = BinaryTree(tree_tuple)
+                return node
+    def __str__(self):
+        return "<{}>".format(self.tuple_from_tree())
 
 
 def main()->None:
     tree_tuple: tuple = ((1,2,None), 5,(7,8,9))
-    tree: BinaryTree = binary_tree_from_tuple(tree_tuple)
-    print(f"In-order traversal 'Left->Root->Right': {in_order_traversal(tree=tree)}")
-    print(f"Post-order traversal 'Left->Right->Root': {post_order_traversal(tree)}")
-    print(f"Pre-order traversal 'Root->Right->Left': {pre_order_traversal(tree)}")
+    tree: BinaryTree = BinaryTree.binary_tree_from_tuple(tree_tuple)
+    print(f"In-order traversal 'Left->Root->Right': {tree.in_order_traversal()}")
+    print(f"Post-order traversal 'Left->Right->Root': {tree.post_order_traversal()}")
+    print(f"Pre-order traversal 'Root->Right->Left': {tree.pre_order_traversal()}")
+    print(f"Height of tree: {tree.height_of_tree()}")
+    print(f"Size of tree: {tree.size_of_tree()}")
+    print(tree)
 
 
 if __name__ =="__main__":
